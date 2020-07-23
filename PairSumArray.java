e63e30894cd1b828d0a87f0662ad878fab69b452
alokbharti <bhartialok.257@gmail.com>
2020-07-24 01:34:40 +0530

    Added PairSumArray solution

--- PairSumArray.java
+++ PairSumArray.java
@@ -3,9 +3,30 @@
 	Expected time complexity - O(n)
 */
 
+import java.util.*;
 class PairSumArray{
 
 	public static void main(String args[]){
-		
+		int a[] = {1, 4, 7, 2, 3, 9, 5};
+		int k = 6;
+		int n = a.length;
+
+		boolean found = false;
+		HashSet<Integer> set = new HashSet();
+		int i=0;
+		for(i=0; i<n;i++){
+			if (set.contains(k-a[i])) {
+				found = true;
+				break;
+			} else {
+				set.add(a[i]);
+			}
+		}
+
+		if (found == true) {
+			System.out.println("Pairs found with sum "+k+" is: ("+a[i]+", "+(k-a[i])+")");
+		} else {
+			System.out.println("No pairs found with sum "+k);
+		}
 	}
 }
